a9b425863fb626dc34da8901a44ea0da4ed8d19c
ARJUN KUMAR <102915876+ERROR404-WEB@users.noreply.github.com>
2023-06-07 21:18:45 +0530

    Day4

---
+++ Day4/2sum.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+vector<vector<int>> pairSum(vector<int> &arr, int s){
+
+vector<vector<int> >ans;
+
+
+    for(int i = 0; i<arr.size(); i++) {
+
+        for(int j = i + 1; j < arr.size(); j++) {
+
+
+          if(arr[i] + arr[j] == s) {
+
+            vector<int> temp;
+
+            temp.push_back(min(arr[i], arr[j]));
+
+            temp.push_back(max(arr[i], arr[j]));
+
+
+            ans.push_back(temp);
+
+          }
+
+        }
+
+   }
+
+        sort(ans.begin(), ans.end());
+
+        return ans;
+
+}
+
+// https://www.codingninjas.com/codestudio/problems/pair-sum_8230699?challengeSlug=striver-sde-challenge&leftPanelTab=1
