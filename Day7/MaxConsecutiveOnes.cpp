69b844a8c582bc719164f895df58c31a4313f137
ARJUN KUMAR <102915876+ERROR404-WEB@users.noreply.github.com>
2023-06-10 23:03:15 +0530

    Day 7 Completed

---
+++ Day7/MaxConsecutiveOnes.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#include <vector>
+
+using namespace std;
+
+ 
+
+int longestSubSeg(vector<int> arr , int n, int k) {
+
+    int i=0,j=0;
+
+    for( i=0;i<n;i++){
+
+        if(arr[i]==0){
+
+            k--;
+
+        }
+
+        if(k<0 && arr[j++]==0){
+
+            k++;
+
+        }
+
+    }
+
+    return i-j;
+
+}
+// https://www.codingninjas.com/codestudio/problems/892994?topList=striver-sde-sheet-problems&leftPanelTab=3
